8547a42f862e9e7d17c71306df8ea6ad4f525d51
Dinmukhamed Nurkaliyev@proton.me <dinmukhamednurkaliyev@proton.me>
2026-09-03 04:01:33 +0500

    chore(server): refine database and TypeScript configuration

--- apps/server/source/ database/ database.ts
+++ apps/server/source/ database/ database.ts
@@ -1,12 +1,12 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-const connectionString = process.env.DATABASE_URL
+const url = process.env.DATABASE_URL
 
-if (!connectionString) {
+if (!url) {
   throw new Error('DATABASE_URL is not defined')
 }
 
-const client = postgres(connectionString)
+const client = postgres(url)
 
 export const database = drizzle(client)
