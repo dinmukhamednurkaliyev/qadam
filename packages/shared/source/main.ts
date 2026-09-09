06d169e69ffdd436e1606dd81d415abb723bc256
Dinmukhamed Nurkaliyev@proton.me <dinmukhamednurkaliyev@proton.me>
2026-09-09 21:48:15 +0500

    feat(authentication): add sign-in contracts

--- packages/shared/source/main.ts
+++ packages/shared/source/main.ts
@@ -1,3 +1,4 @@
+export * from './authentication/sign-in-contracts.js'
 export * from './authentication/sign-up-contracts.js'
 export * from './http/http-contracts.js'
 export * from './vacancy/vacancy-contracts.js'
